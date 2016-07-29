732b43cd26338f935b7ad154f15707020d920af0
Colby Klein <shakesoda@gmail.com>
2016-07-29 02:32:10 -0700

    Update conf for 0.10.0

--- conf.lua
+++ conf.lua
@@ -1,6 +1,14 @@
 function love.conf(t)
 	t.version = "0.10.0"
 	t.window = nil
+	t.gammacorrect    = true
+	t.modules.audio   = false
+	t.modules.sound   = false
 	t.modules.physics = false
 	io.stdout:setvbuf("no")
+end
+
+if love.filesystem.setRequirePath then
+	love.filesystem.setRequirePath(love.filesystem.getRequirePath() .. ";libs/?.lua;libs/?/init.lua")
+	love.filesystem.setRequirePath(love.filesystem.getRequirePath() .. ";src/?.lua;src/?/init.lua")
 end
